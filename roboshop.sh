081d63cdd1e4185727bb305ffa124af6bf09df2b
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-20 17:55:44 +0530

    shell roboshop practice

--- roboshop.sh
+++ roboshop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-Ami_ID=ami-09c813fb71547fc4f
-SG_ID=sg-0bc221ac9b59215f1
+Ami_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0bc221ac9b59215f1"
 
 for instance in $@
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $Ami_ID --instance-type t3.micro $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $Ami_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
