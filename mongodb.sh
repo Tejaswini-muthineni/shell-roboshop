2a4843438d32fefd6ea3d810a17eb843895cc5e8
tejaswini <muthinenitejaswini9@gmail.com>
2025-10-21 19:54:18 +0530

    mongodb conf

--- mongodb.sh
+++ mongodb.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
-USER=$( id  -u )
+
+USERID=$(id -u)
 R="\e[31m"
-N="\e[0m"
 G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-if [ $USER -ne 0 ]; then
-    echo " take root user access"
-    exit 1
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE()
-{
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "$2 .... $R failed $N"
-    else 
-        echo -e "$2 ...$G success $N"
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-dnf install mongodb-org -y 
-VALIDATE $? "Installing mongodb"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
 
-systemctl enable mongod 
-VALIDATE $? "enabling mongodb"
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable MongoDB"
 
 systemctl start mongod 
-VALIDATE $? "starting mongodb"
+VALIDATE $? "Start MongoDB"
 
-sed -i 's/127.0.0.1/ 0.0.0.0' /etc/mongod.conf
-VALIDATE $? "Update listen address"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod
-VALIDATE $? "restart mangodb"
+VALIDATE $? "Restarted MongoDB"
